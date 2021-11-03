1f970ec56bd6c59ca10f82feb88658e57574d6a4
Donna 'Katie' McCullough <dmccull8@vols.utk.edu>
2021-11-02 23:20:01 -0400

    first add

---
+++ scripts/All_Hepes_trials.py
@@ -0,0 +1,176 @@
+'''
+All Hepes trials from Fig 1_a
+
+Trying to match HOOH production data from Morris et al 2011 using an analytical solution, euler's aproximation, and ODEint 
+
+Goal is to have all Hepes data shown and then have analytical solutions for all that differ only in production (S_hooh)terms...with decay staying the same for all.
+
+Using Morrisand Zinser 2013 Fig 1_a  data about HOOH production from Hepes buffer to get a 'productuion rate of HOOH vi Hepes buffer'  
+
+created by DKM
+
+location:/Users/dkm/Documents/Talmy_research/Zinser_and_Ben/Project_5_Hepes_Morris_2013/scripts/
+'''
+
+
+from scipy import *
+from scipy.integrate import *
+import pandas as pd
+import numpy as np      
+import matplotlib.pyplot as plt   
+
+
+
+
+######################################################################
+
+#  Importing the data as dfs for each trial
+
+######################################################################
+
+#####   10 Hepes
+Hepes_10_df = pd.read_excel('../data/data_fig1a_Morris_and_Zinser_2013.xlsx', header = 2)   #importing data while ignoring 2 header rows...from first sheet of excel file
+Hepes_10_df.columns = ['Time (days)', 'HOOH Concentration (\u03BCM)']     #renaming columns of dataframe so they are labeled appropriately 
+
+######  1 Hepes
+Hepes_1_df = pd.read_excel('../data/data_fig1a_Morris_and_Zinser_2013.xlsx','1 mM Hepes', header = 2)   #importing data for 1.0 trial 
+Hepes_1_df.columns = ['Time (days)', 'HOOH Concentration (\u03BCM)']     
+
+ 
+###### 0.1 Hepes
+
+Hepes_01_df = pd.read_excel('../data/data_fig1a_Morris_and_Zinser_2013.xlsx','0.1mM Hepes', header = 2)   #importing data for 0.1 trial
+Hepes_01_df.columns = ['Time (days)', 'HOOH Concentration (\u03BCM)']    
+
+#######   0 Hepes
+
+Hepes_0_df = pd.read_excel('../data/data_fig1a_Morris_and_Zinser_2013.xlsx','0mM Hepes', header = 2)   #importing data for 0 trial 
+Hepes_0_df.columns = ['Time (days)', 'HOOH Concentration (\u03BCM)']     
+
+
+
+
+plt.scatter(Hepes_10_df[['Time (days)']],Hepes_10_df[['HOOH Concentration (\u03BCM)']], marker = 'x', s=50, c = 'r', label = 'HOOH from 10\u03BCM Hepes')
+plt.scatter(Hepes_1_df[['Time (days)']],Hepes_1_df[['HOOH Concentration (\u03BCM)']], marker = 'x', s=50, c = 'b', label = 'HOOH from 1.0\u03BCM Hepes')
+plt.scatter(Hepes_01_df[['Time (days)']],Hepes_01_df[['HOOH Concentration (\u03BCM)']], marker = 'x', s=50, c = 'g', label = 'HOOH from 0.1\u03BCM Hepes')
+plt.scatter(Hepes_0_df[['Time (days)']],Hepes_0_df[['HOOH Concentration (\u03BCM)']], marker = 'x', s=50, c = 'y', label = 'HOOH from 0\u03BCM Hepes')
+
+
+plt.xlabel('Time (day)', fontsize = 16)
+plt.ylabel('HOOH concentration (\u03BCM)', fontsize = 16)
+plt.yscale('log')
+plt.tick_params(labelsize = 12) 
+#plt.show()
+
+
+
+####################################
+
+ # Units for all analytical solutions
+
+####################################
+
+delta = 1.6    
+step = 0.05 #delta t
+ndays = 26
+times = np.linspace(0,ndays,int(ndays/step))
+
+
+
+
+####################################
+
+#analytical solution for 10
+
+####################################
+
+#initial values and creating time array
+
+S_HOOH = 15
+
+def f(t, S_HOOH, delta):
+        H = (S_HOOH/delta)*(1-e**(-delta*t))
+        return H
+
+Hs = f(times,S_HOOH,delta)
+#print(times,Hs) 
+
+
+plt.plot(times,Hs,c='r',marker='.',label='10\u03BCM Hepes Analytical Solution')
+
+
+
+
+####################################
+
+#analytical solution for 1
+
+####################################
+
+#initial values and creating time array
+
+S_HOOH = 7.8
+
+def f(t, S_HOOH, delta):
+        H = (S_HOOH/delta)*(1-e**(-delta*t))
+        return H
+
+Hs = f(times,S_HOOH,delta)
+#print(times,Hs) 
+
+
+plt.plot(times,Hs,c='b',marker='.',label='1\u03BCM Hepes Analytical Solution')
+
+
+
+
+####################################
+
+#analytical solution for 0.1
+
+####################################
+
+#initial values and creating time array
+
+S_HOOH = 3.4
+
+def f(t, S_HOOH, delta):
+        H = (S_HOOH/delta)*(1-e**(-delta*t))
+        return H
+
+Hs = f(times,S_HOOH,delta)
+#print(times,Hs) 
+
+
+plt.plot(times,Hs,c='g',marker='.',label='0.1\u03BCM Hepes Analytical Solution')
+
+
+
+
+####################################
+
+#analytical solution for 00
+
+####################################
+
+#initial values and creating time array
+
+S_HOOH = 1.0
+
+def f(t, S_HOOH, delta):
+        H = (S_HOOH/delta)*(1-e**(-delta*t))
+        return H
+
+Hs = f(times,S_HOOH,delta)
+#print(times,Hs) 
+
+
+plt.plot(times,Hs,c='y',marker='.',label='0\u03BCM Hepes Analytical Solution')
+
+
+
+
+
+
+plt.legend()
+plt.show()
---
+++ scripts/Hepes_production_of_hooh.py
@@ -0,0 +1,169 @@
+'''
+Hepes_production_of_hooh.py
+
+Trying to match HOOH production data from Morris et al 2011 using an analytical solution, euler's aproximation, and ODEint 
+
+Using Morrisand Zinser 2013 Fig 1_a  data about HOOH production from Hepes buffer to get a 'productuion rate of HOOH vi Hepes buffer'  
+
+created by DKM
+
+location:/Users/dkm/Documents/Talmy_research/Zinser_and_Ben/Project_3_Hepes_Morris_2013/scripts/
+'''
+
+
+from scipy import *
+from scipy.integrate import *
+import pandas as pd
+import numpy as np      
+import matplotlib.pyplot as plt   
+
+
+
+
+###############################
+
+#  Importing the data frame
+
+###############################
+
+ 
+#xls = pd.read_excel('../data/data_fig1a_Morris_and_Zinser_2013.xlsx')
+hepes_10_df = pd.read_excel('../data/data_fig1a_Morris_and_Zinser_2013.xlsx','10mM Hepes',delimiter = '.', header = 2, names = ( 'Time (days)', 'HOOH Concentration (\u03BCM)')
+#10mM_HOOH_df = pd.read_csv('../data/data_fig1a_Morris_and_Zinser_2013', delimiter =',', header= 2, names =( 'Time (1/days)','HOOH concentration'))
+
+
+
+'''
+##############################
+
+#   data arrays
+
+###############################
+
+HOOH_data = 10**np.array(HOOH_df.iloc[:,1])   #raised to 10 because ot the way data thief recorded the data and its scale
+
+#print(HOOH_data)
+
+
+
+
+
+##############################
+
+#   time arrays
+
+##############################
+
+
+HOOH_times =np.array(HOOH_df.iloc[:,0])
+#print(HOOH_times)
+
+
+
+
+
+##############################
+
+#    graphing the data 
+
+##############################
+
+
+plt.scatter(HOOH_times, HOOH_data, marker = 'x', s=50, c = 'r', label = 'Measured HOOH') 
+plt.xlabel('Time (day $^{-1}$)', fontsize=16) 
+plt.ylabel('HOOH concentration (\u03BCM)',fontsize=16)
+plt.yscale('log')
+plt.tick_params(labelsize=12)
+#plt.show()
+
+
+Hs = HOOH_data
+Ts = HOOH_times
+
+Hsd = np.std(Hs)     #getting numpy to find the stdv of the data (but really will probs be 0.1 bc the data set doesn't have triplicates in it. 
+
+
+
+
+####################################
+
+#analytical solution
+
+####################################
+
+#initial values and creating time array
+
+delta = 0.6
+S_HOOH = 2.8
+step = 0.05 #delta t
+ndays = 7
+times = np.linspace(0,ndays,int(ndays/step))
+
+def f(t, S_HOOH, delta):
+	H = (S_HOOH/delta)*(1-e**(-delta*t))
+	return H
+
+Hs = f(times,S_HOOH,delta)
+#print(times,Hs) 
+
+
+#plt.plot(times,Hs,c='g',marker='*',label='Analytical Solution')
+
+
+
+
+
+
+#############################
+
+#Euler's Integration
+
+############################
+
+HsEuler = np.array([]) 
+H = 0.0
+t0 = 0
+
+for t in times: 
+	HsEuler = np.append(HsEuler,H)
+	dHdt = S_HOOH - delta*H
+	H = H + dHdt*step
+	
+#plt.plot(times,HsEuler,c='k',label = "Euler's Aproximation")
+
+plt.legend()
+
+#plt.show()
+
+
+
+####################################
+
+#ODE int
+
+####################################
+
+
+from scipy.integrate import odeint
+
+def HsODEint(H,t):
+	dHdt = S_HOOH-delta*H
+	#print(dHdt,t,H)
+	return dHdt
+
+
+ode_solutions = odeint(HsODEint,0,times)
+
+
+#plt.plot(times,ode_solutions,c='purple', linestyle = ':', label = 'Odeint Approximation')
+
+plt.legend()
+
+plt.show()
+
+
+'''
+
+
+print('Done')
+
